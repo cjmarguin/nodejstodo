00dcf1ad9d1d2377927bce614c36d73dec218dff
JoseJCanedo <53786534+JoseJCanedo@users.noreply.github.com>
2019-10-15 10:53:47 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+const http = require('http');
 const express = require('express');
 const bodyParser = require("body-parser");
-//const path = require('path');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-//app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -36,6 +37,6 @@ app.post("/removetask", function(req, res) {
    res.redirect("/");
 });
 
-app.listen(port, function(){
+http.createServer(app).listen(port, function(){
    console.log('example app' + port);
 });
