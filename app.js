aee1e2a76e30f5d0326b0bdb6f99ea53a5721d21
JoseJCanedo <53786534+JoseJCanedo@users.noreply.github.com>
2019-10-15 11:00:51 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-const http = require('http');
-const express = require('express');
-const bodyParser = require("body-parser");
-const path = require('path');
-const app = express();
-const port = process.env.PORT || 3000;
+var http = require('http');
+var express = require('express');
+var bodyParser = require("body-parser");
+var path = require('path');
+var app = express();
+var port = process.env.PORT || 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const task = ["buy socks", "practise with nodejs"];
-const complete = ["finish jquery"];
+var task = ["buy socks", "practise with nodejs"];
+var complete = ["finish jquery"];
 
 app.get("/", function(req, res) {
     res.render("index", { task: task});
 });
 
 app.post('/addtask', function (req, res) {
-    const newTask = req.body.newtask;
+    var newTask = req.body.newtask;
     task.push(newTask);
     res.redirect("/");
 });
 
 app.post("/removetask", function(req, res) {
-    const completeTask = req.body.check;
+    var completeTask = req.body.check;
     if (typeof completeTask === "string") {
-     complete.push(completeTask);
+      complete.push(completeTask);
       task.splice(task.indexOf(completeTask), 1);
     } else if (typeof completeTask === "object") {
       for (var i = 0; i < completeTask.length; i++) {     
