c67d3fbd3ecaa9352530c2e462be692cb17e3dc1
JoseJCanedo <53786534+JoseJCanedo@users.noreply.github.com>
2019-10-15 10:19:20 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-var bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -8,21 +8,21 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var task = ["buy socks", "practise with nodejs"];
-var complete = ["finish jquery"];
+const task = ["buy socks", "practise with nodejs"];
+const complete = ["finish jquery"];
 
 app.get("/", function(req, res) {
     res.render("index", { task: task});
 });
 
 app.post('/addtask', function (req, res) {
-    var newTask = req.body.newtask;
+    const newTask = req.body.newtask;
     task.push(newTask);
     res.redirect("/");
 });
 
 app.post("/removetask", function(req, res) {
-    var completeTask = req.body.check;
+    const completeTask = req.body.check;
     if (typeof completeTask === "string") {
      complete.push(completeTask);
       task.splice(task.indexOf(completeTask), 1);
