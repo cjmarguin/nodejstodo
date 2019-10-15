7a4aaadd558b7829d1106ecf832dc3aa72bacbd5
JoseJCanedo <53786534+JoseJCanedo@users.noreply.github.com>
2019-10-15 10:50:23 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-const path = require('path');
+//const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.set('views', path.join(__dirname, 'views'));
+//app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
